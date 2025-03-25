fa838eb7a9e46ef7902c58d88e2a2bf12fc03848
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-25 17:21:19 +0000

    code changed

--- answer.php
+++ answer.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 
+
 <head>
   <meta charset="utf-8" />
   <meta name="description" content="Salary, in PHP" />
@@ -9,13 +10,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-deep_orange.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-amber.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>Salary,in PHP</title>
+  <title>Salary, in PHP</title>
 </head>
+
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
@@ -27,21 +29,21 @@
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/income_tax.jpg" alt="income_tax" width="250" />
+        <img src="./images/income_tax.jpg" alt="income tax image" width="300" />
       </div>
       <div class="page-content-php">
-        <div id="incometax-info">
+        <div id="employment-info">
           <?php
-          //tax rate 
+          // tax rate
           const TAX_RATE = 0.18;
 
-          //input
+          // input
           $hoursWorked = $_GET["hours-worked"];
           $hourlyWage = $_GET["hourly-wage"];
 
           // process
           $pay = ($hoursWorked * $hourlyWage) * (1.00 - TAX_RATE);
-          $taxes = ($hoursWorked * $hourlyWage) * TAX_RATE;
+          $taxes = $hoursWorked * $hourlyWage * TAX_RATE;
 
           // output
           echo "Your pay will be: $" . (round($pay, 2)) . "<br />";
@@ -50,7 +52,7 @@
         </div>
       </div>
       <div class="page-content-return">
-        <a href="./index.php">Return...</a>
+        <a href="./index.php">Return ...</a>
       </div>
     </main>
   </div>
